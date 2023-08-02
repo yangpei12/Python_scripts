cef1d7073314cc6a66d11934a261662fb1b270ec
peiy <695819143@qq.com>
2023-08-02 23:17:36 +0800

    滑动窗口

---
+++ .history/生物信息数据分析/滑动窗口_20230802231341.py
@@ -0,0 +1,14 @@
+# 滑动窗口
+sep = [1,2,3,4] 
+# n表示窗口的长度，m表示从序列的哪个位置开始
+def outter_function(n):
+    def inner_function(m):
+        return sep[m:m+n]
+    return inner_function
+
+a = outter_function(2)
+b = a(1)
+
+
+
+
